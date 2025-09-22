6bb70c6d1614ed4e3c37c02e3e381435342b1ebe
dean <deanfernandes18@gmail.com>
2025-09-22 19:26:36 +0100

    add products, wishlist routes

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import addressesRouter from "./addresses.js";
-import paymentMethodsRouter from "./payment-methods.js";
-import wishlistRouter from "./wishlist.js";
 
 const router = Router();
+
 router.post("/", (req, res) => {});
 router.get("/", (req, res) => {});
 router.get("/:id", (req, res) => {});
 router.put("/:id", (req, res) => {});
 router.patch("/:id", (req, res) => {});
 router.delete("/:id", (req, res) => {});
-router.use("/:userId/addresses", addressesRouter);
-router.use("/:userId/payment-methods", paymentMethodsRouter);
-router.use("/:userId/wishlist", wishlistRouter);
 
 export default router;
