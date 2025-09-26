2ecf4f2b812dcd9955d93ad074146fa153f8096a
dean <deanfernandes18@gmail.com>
2025-09-26 12:07:22 +0100

    add order delivery options endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth.js";
 import usersRouter from "./users/users.js";
 import productsRouter from "./products/products.js";
 import productCategoriesRouter from "./productCategories.js";
+import orderDeliveryOptionsRouter from "./orderDeliveryOptions.js";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRouter);
 router.use("/users", usersRouter);
 router.use("/products", productsRouter);
 router.use("/product-categories", productCategoriesRouter);
+router.use("/order-delivery-options", orderDeliveryOptionsRouter);
 
 export default router;
