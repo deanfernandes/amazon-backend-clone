331d0210451b2e094fb7cdd73e57b238e3ffc96b
dean <deanfernandes18@gmail.com>
2025-09-26 10:32:36 +0100

    add migration

---
+++ db/migrations/009_add_user_orders_products_join_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_order_products (
+    user_order_id BIGINT NOT NULL REFERENCES user_orders(id) ON DELETE CASCADE,
+    product_id BIGINT NOT NULL REFERENCES products(id),
+    product_price NUMERIC(10, 2) NOT NULL CHECK(product_price > 0),
+    quantity SMALLINT NOT NULL CHECK(quantity > 0),
+    PRIMARY KEY (user_order_id, product_id)
+);
