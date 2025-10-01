cb1eb438e1b63db2a7475f8fe112039c2fff728a
dean <deanfernandes18@gmail.com>
2025-10-01 16:58:35 +0100

    add swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import indexRouter from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "node:fs";
+import YAML from "yaml";
+
+const file = fs.readFileSync("./openapi.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
 
 const app = express();
 app.use(express.json());
 app.use("/api/v1", indexRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
